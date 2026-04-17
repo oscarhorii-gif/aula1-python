d78a06d09aff25fbeb549f2d08c47c24d8abf27f
Oscar Horii <oscar.horii@gmail.com>
2026-04-17 07:39:05 -0300

    Primeiro programa com input

---
+++ main.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+cidade = input("Digite sua cidade: ")
+ano_nascimento = int(input("Digite seu ano de nascimento: "))
+
+idade = 2026 - ano_nascimento
+
+print("Olá,", nome, "de", cidade)
+print("Você tem", idade, "anos")
